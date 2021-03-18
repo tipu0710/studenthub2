cfaaafe040d2b92b236ed5ab0ca890424b52a4a9
Tipu Sultan <tipu@codesign.com.bd>
2021-03-18 16:47:16 +0600

    settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:studenthub2/global.dart';
-import 'ui/notification/view/notification.dart';
+import 'package:studenthub2/ui/settings/view/settings.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Color(0xfffcfcfc)
       ),
       debugShowCheckedModeBanner: false,
-      home: Notifications(),
+      home: Settings(),
     );
   }
 }
---
+++ lib/ui/settings/view/settings.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:studenthub2/ui_helper/ui_helper.dart';
+
+class Settings extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(height: 164,),
+                  title('Quick Link'),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  childCard("Uni Website"),
+                  childCard("Uni Facebook"),
+                  childCard("Uni Instagram"),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  title('Help & Support'),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  childCard("Contact Us"),
+                  childCard("Uni Facebook"),
+                  childCard("Uni Instagram"),
+                  SizedBox(
+                    height: 40,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              height: 30,
+              color: Color(0xfffcfcfc),
+            ),
+          ),
+          UiHelper().back(context, title: "Settings"),
+        ],
+      ),
+    );
+  }
+
+  Widget childCard(String title) {
+    return Container(
+      height: 50.0,
+      margin: EdgeInsets.only(top: 10),
+      padding: EdgeInsets.only(left: 20, right: 20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5.0),
+        color: const Color(0xffffffff),
+        border: Border.all(width: 1.0, color: const Color(0xffeeeeee)),
+      ),
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 15,
+                color: const Color(0xff585858),
+              ),
+            ),
+            Icon(Icons.arrow_forward_ios, color: Color(0xffACACAC), size: 14,)
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget title(String title) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.only(left: 20,),
+      child: Text(
+        title,
+        style: TextStyle(
+          fontFamily: 'Roboto',
+          fontSize: 18,
+          color: const Color(0xff373747),
+          fontWeight: FontWeight.w500,
+        ),
+        textHeightBehavior: TextHeightBehavior(applyHeightToFirstAscent: false),
+        textAlign: TextAlign.left,
+      ),
+    );
+  }
+}
